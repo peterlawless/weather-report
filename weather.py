877945eebffe2429c42ab8a1ca822a73b3b8436a
Peter Lawless <peterlawless223@gmail.com>
2016-09-07 15:42:51 -0400

    Created 'WeatherReport' object, wrote methods to retrieve both current conditions and ten day forecast

--- weather.py
+++ weather.py
@@ -1,7 +1,34 @@
 from secret import api_key
 import requests
-import requests_mock
 import re
+
+
+class WeatherReport:
+    def __init__(self, zip_code, api_key):
+        self.zip_code = zip_code
+        self.api_key = api_key
+
+    def get_json(self, endpoint):
+        base = "http://api.wunderground.com/api/"
+        url = base + api_key + "/" + endpoint + "/q/zmw:" + self.zip_code + ".1.99999.json"
+        return requests.get(url).json()
+
+    def get_current_conditions(self):
+        data = self.get_json('conditions')['current_observation']
+        print(data['display_location']['full'])
+        print(data['weather'], "with a visibility of ",
+              data['visibility_mi'], " miles.")
+        print("Wind is blowing ", data['wind_dir'], "at ",
+              data['wind_mph'], "mph with gusts reaching ",
+              data['wind_gust_mph'], " mph.")
+        print("Temperature: ", data['temperature_string'])
+        print()
+
+    def get_ten_day_forecast(self):
+        data = self.get_json('forecast10day')['forecast']['txt_forecast']['forecastday']
+        for item in data:
+            print(item['title'], ":")
+            print(item['fcttext'], "\n")
 
 
 def main():
@@ -12,13 +39,21 @@ def main():
             continue
         else:
             break
-    base = "http://api.wunderground.com/api/"
-    url = base + api_key + "/conditions/q/zmw:" + zip_code + ".1.99999.json"
-
-    x = requests.get(url).json()['current_observation']
-    print("Wind is blowing ", x['wind_dir'], "at ", x['wind_mph'], "mph \
-with gusts reaching ", x['wind_gust_mph'], " mph.")
-    print("Temperature: ",x['temperature_string'])
+    while True:
+        print("Please select your report: ")
+        print("1. Current Conditions")
+        print("2. Ten Day Forecast. \n")
+        selection = input('>>>')
+        if selection not in ['1', '2']:
+            print("Invalid input.")
+            continue
+        else:
+            break
+    weather_report = WeatherReport(zip_code, api_key)
+    if selection == '1':
+        weather_report.get_current_conditions()
+    elif selection == '2':
+        weather_report.get_ten_day_forecast()
 
 if __name__ == '__main__':
     main()
